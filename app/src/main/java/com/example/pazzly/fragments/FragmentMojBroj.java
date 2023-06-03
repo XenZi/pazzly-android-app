be7e136e1876efbdbf2c7968684552c3d84e20c7
XenZi <rilakmateja@gmail.com>
2023-06-03 13:01:36 +0200

    Initializing basic info of the application

--- app/src/main/java/com/example/pazzly/fragments/FragmentMojBroj.java
+++ app/src/main/java/com/example/pazzly/fragments/FragmentMojBroj.java
@@ -2,6 +2,7 @@ package com.example.pazzly.fragments;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,17 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+
 import com.example.pazzly.R;
 
+import org.apache.commons.jexl3.JexlBuilder;
+import org.apache.commons.jexl3.JexlContext;
+import org.apache.commons.jexl3.JexlEngine;
+import org.apache.commons.jexl3.JexlExpression;
+import org.apache.commons.jexl3.MapContext;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -21,11 +31,15 @@ public class FragmentMojBroj extends Fragment {
     private View view;
     private TextView wantedNumberTextView;
     private Button stopButton;
+    private Button deleteButton;
+    private Button submitButton;
     private Handler handler;
     private Random random;
     private boolean isUpdating;
     private AtomicInteger clickCounter;
     private TextView currentStateOfExpression;
+    private List<Button> numberButtons;
+    private List<Button> operationButtons;
 
     public static FragmentMojBroj newInstance() {
         return new FragmentMojBroj();
@@ -35,6 +49,8 @@ public class FragmentMojBroj extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_moj_broj, container, false);
         initializeViews();
+        initializeNumberButtons();
+        initializeOperationButtons();
         initializeListeners();
         initializeValues();
 
@@ -50,45 +66,76 @@ public class FragmentMojBroj extends Fragment {
     private void initializeViews() {
         wantedNumberTextView = view.findViewById(R.id.wantedNumber);
         stopButton = view.findViewById(R.id.stopMojBroj);
+        deleteButton = view.findViewById(R.id.deleteExpressionButton);
+        submitButton = view.findViewById(R.id.btnSubmitMojBroj);
         currentStateOfExpression = view.findViewById(R.id.currentStateOfExpression);
+    }
+
+    private void initializeNumberButtons() {
+        numberButtons = new ArrayList<>();
+        numberButtons.add(view.findViewById(R.id.randomNr1));
+        numberButtons.add(view.findViewById(R.id.randomNr2));
+        numberButtons.add(view.findViewById(R.id.randomNr3));
+        numberButtons.add(view.findViewById(R.id.randomNr4));
+        numberButtons.add(view.findViewById(R.id.randomNr5));
+        numberButtons.add(view.findViewById(R.id.randomNr6));
+    }
+
+    private void initializeOperationButtons() {
+        operationButtons = new ArrayList<>();
+        operationButtons.add(view.findViewById(R.id.opAdd));
+        operationButtons.add(view.findViewById(R.id.opSubtract));
+        operationButtons.add(view.findViewById(R.id.opMultiply));
+        operationButtons.add(view.findViewById(R.id.opDivide));
+        operationButtons.add(view.findViewById(R.id.opOpenBracket));
+        operationButtons.add(view.findViewById(R.id.opClosedBracket));
     }
 
     private void initializeListeners() {
         stopButton.setOnClickListener(v -> handleStopButtonClick());
+        deleteButton.setOnClickListener(v -> handleDeleteLastInputFromExpression());
+        submitButton.setOnClickListener(v -> handleSubmit());
         setNumberButtonListeners();
         setOperationButtonListeners();
     }
 
     private void setNumberButtonListeners() {
-        Button randomNumberButton1 = view.findViewById(R.id.randomNr1);
-        Button randomNumberButton2 = view.findViewById(R.id.randomNr2);
-        Button randomNumberButton3 = view.findViewById(R.id.randomNr3);
-        Button randomNumberButton4 = view.findViewById(R.id.randomNr4);
-        Button randomNumberButton5 = view.findViewById(R.id.randomNr5);
-        Button randomNumberButton6 = view.findViewById(R.id.randomNr6);
+        for (Button button : numberButtons) {
+            setNumberClickListener(button);
+        }
+    }
 
-        randomNumberButton1.setOnClickListener(v -> updateExpressionText(randomNumberButton1.getText().toString()));
-        randomNumberButton2.setOnClickListener(v -> updateExpressionText(randomNumberButton2.getText().toString()));
-        randomNumberButton3.setOnClickListener(v -> updateExpressionText(randomNumberButton3.getText().toString()));
-        randomNumberButton4.setOnClickListener(v -> updateExpressionText(randomNumberButton4.getText().toString()));
-        randomNumberButton5.setOnClickListener(v -> updateExpressionText(randomNumberButton5.getText().toString()));
-        randomNumberButton6.setOnClickListener(v -> updateExpressionText(randomNumberButton6.getText().toString()));
+
+    private void setNumberClickListener(Button button) {
+        button.setOnClickListener(v -> {
+            String currentExpression = currentStateOfExpression.getText().toString();
+            if (currentExpression.length() == 0) {
+                updateExpressionText(button.getText().toString());
+            }
+            if (currentExpression.length() > 0 && hasOperationBefore(currentExpression)) {
+                updateExpressionText(button.getText().toString());
+            }
+            button.setEnabled(false);
+        });
+    }
+
+    private boolean hasOperationBefore(String expression) {
+        // Check if the expression ends with an operation symbol
+        return expression.endsWith("+") ||
+                expression.endsWith("-") ||
+                expression.endsWith("*") ||
+                expression.endsWith("/") ||
+                expression.endsWith("(");
     }
 
     private void setOperationButtonListeners() {
-        Button opAdd = view.findViewById(R.id.opAdd);
-        Button opSubtract = view.findViewById(R.id.opSubtract);
-        Button opMultiply = view.findViewById(R.id.opMultiply);
-        Button opDivide = view.findViewById(R.id.opDivide);
-        Button opOpenBracket = view.findViewById(R.id.opOpenBracket);
-        Button opClosedBracket = view.findViewById(R.id.opClosedBracket);
+        for (Button button : operationButtons) {
+            setOperationClickListener(button);
+        }
+    }
 
-        opAdd.setOnClickListener(v -> updateExpressionText("+"));
-        opSubtract.setOnClickListener(v -> updateExpressionText("-"));
-        opMultiply.setOnClickListener(v -> updateExpressionText("*"));
-        opDivide.setOnClickListener(v -> updateExpressionText("/"));
-        opOpenBracket.setOnClickListener(v -> updateExpressionText("("));
-        opClosedBracket.setOnClickListener(v -> updateExpressionText(")"));
+    private void setOperationClickListener(Button button) {
+        button.setOnClickListener(v -> updateExpressionText(button.getText().toString()));
     }
 
     private void initializeValues() {
@@ -165,5 +212,55 @@ public class FragmentMojBroj extends Fragment {
 
     private void updateExpressionText(String text) {
         currentStateOfExpression.append(text);
+    }
+
+    private void handleDeleteLastInputFromExpression() {
+        String currentExpression = currentStateOfExpression.getText().toString();
+
+        if (currentExpression.isEmpty()) {
+            return;
+        }
+        if (isLastInputNumber(currentExpression)) {
+            int lastNumberStart = findLastNumberStart(currentExpression);
+            String updatedExpression = currentExpression.substring(0, lastNumberStart);
+            currentStateOfExpression.setText(updatedExpression);
+            enableNumberButton(currentExpression.substring(lastNumberStart, currentExpression.length()));
+        } else {
+            currentStateOfExpression.setText(currentExpression.substring(0, currentExpression.length() - 1));
+        }
+    }
+
+    private boolean isLastInputNumber(String expression) {
+        char lastChar = expression.charAt(expression.length() - 1);
+        return Character.isDigit(lastChar);
+    }
+
+    private void enableNumberButton(String number) {
+        numberButtons.forEach(button -> {
+            if (button.getText().toString().equals(number)) {
+                button.setEnabled(true);
+            }
+        });
+    }
+    private int findLastNumberStart(String expression) {
+        int i = expression.length() - 1;
+        while (i >= 0 && Character.isDigit(expression.charAt(i))) {
+            i--;
+        }
+        return i + 1;
+    }
+
+    private void handleSubmit() {
+        String expression = currentStateOfExpression.getText().toString();
+        TextView finalResult = view.findViewById(R.id.resultFromUser1);
+        JexlEngine jexlEngine = new JexlBuilder().create();
+        JexlContext jexlContext = new MapContext();
+        try {
+            JexlExpression jexlExpression = jexlEngine.createExpression(expression);
+            Object result = jexlExpression.evaluate(jexlContext);
+            finalResult.setText(String.valueOf(result));
+        } catch (Exception e) {
+            finalResult.setText("Invalid");
+        }
     }
 }
